913f589e67a817f63d16582787762d75fc14eeae
vipul <vipulkumar2426@gmail.com>
2024-02-13 00:42:52 +0530

    some changes 2

--- gsc-frontend/src/components/TeamCard.jsx
+++ gsc-frontend/src/components/TeamCard.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./TeamCard.css";
 import { Gmail } from "@styled-icons/boxicons-logos/Gmail";
 import { Instagram } from "@styled-icons/boxicons-logos/Instagram";
-import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin";
-import { Facebook } from "@styled-icons/boxicons-logos";
-import { Twitter } from "@styled-icons/boxicons-logos";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 function TeamCard(props) {
   // console.log(props.image);
   return (
-    <div className="cardbox shadow-lg" style={{ backgroundImage: `url(${props.image})` }}>
+    <div className="cardbox" style={{ backgroundImage: `url(${props.image})` }}>
+      
+      <h2 className="card-title">{props.name}</h2>
       <div className="card-content">
-        <h2 className="card-title">{props.name}</h2>
+       
 
         <div className="btn">
         <a
@@ -18,27 +21,28 @@ function TeamCard(props) {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Instagram className=" h-8 text-white hover:text-pink-400"></Instagram>
-          </a>
-          <a  href={"//" + props.facebook}
-            target="_blank"
-            rel="noopener noreferrer">
-            <Facebook className=" h-8 text-white hover:text-red"></Facebook>
-          </a>
-          <a
-            href={"//" + props.linkedin}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Linkedin className=" h-8 text-white hover:text-blue"></Linkedin>
+            <Instagram className=" sm:h-12 h-10 sm:text-5xl text-3xl text-white hover:text-pink-400"></Instagram>
           </a>
           <a
             href={"//" + props.twitter}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Twitter className=" h-8 text-white hover:text-pink-400"></Twitter>
+            <FaXTwitter className=" sm:h-12 sm:text-5xl text-3xl h-10 text-white hover:text-pink-400"></FaXTwitter>
           </a>
+          <a  href={"//" + props.facebook}
+            target="_blank"
+            rel="noopener noreferrer">
+            <FaFacebook className=" sm:h-12 sm:text-5xl text-3xl h-10 text-white hover:text-pink-400"></FaFacebook>
+          </a>
+          <a
+            href={"//" + props.linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin  className=" sm:h-12 h-10 sm:text-5xl text-3xl text-white hover:text-pink-400 "></FaLinkedin>
+          </a>
+          
         </div>
       </div>
     </div>
--- gsc-frontend/src/pages/Sports.jsx
+++ gsc-frontend/src/pages/Sports.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import TeamCard from "../components/TeamCard";
-
+import cricket from "../assets/cricket.png"
 function Sports() {
   const [scrolling, setScrolling] = useState(false);
   const data = [
     {
       name: "Cricket",
-      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      imageUrl: cricket,
       email: "john.doe@example.com",
       instagram: "john_doe_instagram",
       linkedin: "john_doe_linkedin",
@@ -15,7 +15,7 @@ function Sports() {
       facebook:"facebook"
     },{
       name: "Cricket",
-      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      imageUrl: cricket,
       email: "john.doe@example.com",
       instagram: "john_doe_instagram",
       linkedin: "john_doe_linkedin",
@@ -23,7 +23,7 @@ function Sports() {
       facebook:"facebook"
     },{
       name: "Cricket",
-      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      imageUrl: cricket,
       email: "john.doe@example.com",
       instagram: "john_doe_instagram",
       linkedin: "john_doe_linkedin",
@@ -31,7 +31,7 @@ function Sports() {
       facebook:"facebook"
     },{
       name: "Cricket",
-      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      imageUrl: cricket,
       email: "john.doe@example.com",
       instagram: "john_doe_instagram",
       linkedin: "john_doe_linkedin",
@@ -39,7 +39,87 @@ function Sports() {
       facebook:"facebook"
     },{
       name: "Cricket",
-      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+      twitter:"twitter",
+      facebook:"facebook"
+    },{
+      name: "Cricket",
+      imageUrl: cricket,
       email: "john.doe@example.com",
       instagram: "john_doe_instagram",
       linkedin: "john_doe_linkedin",
@@ -73,17 +153,19 @@ function Sports() {
         <div
           style={{
             position: "absolute",
-            top: "31%",
+            top: "32%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            fontSize: "16vw",
+            fontSize: "24vw",
             color: "antiquewhite",
-            opacity:'0.7'
+            opacity:'0.7',
+            fontFamily:'lexend zetha',
+            fontWeight:"bold"
           }}
         >
           Sports
         </div>
-        <div className="flex items-center flex-wrap justify-center gap-20 pt-60 pb-20 px-10">
+        <div className="flex items-center flex-wrap justify-center sm:gap-20 pt-72 pb-20 px-10">
           {data.map((member) => (
             <div>
               <TeamCard
