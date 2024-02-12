0defc627d41e8defa90d272e9a6912c72d8438f5
vipul <vipulkumar2426@gmail.com>
2024-02-12 05:32:29 +0530

    Create Sports Page

--- gsc-frontend/src/pages/Sports.jsx
+++ gsc-frontend/src/pages/Sports.jsx
@@ -1,8 +1,42 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
+import TeamCard from "../components/TeamCard";
 
 function Sports() {
   const [scrolling, setScrolling] = useState(false);
+  const data = [
+    {
+      name: "Cricket",
+      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+    },{
+      name: "Cricket",
+      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+    },{
+      name: "Cricket",
+      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+    },{
+      name: "Cricket",
+      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+    },{
+      name: "Cricket",
+      imageUrl: "https://c8.alamy.com/comp/CC2TRA/cricket-player-batting-CC2TRA.jpg",
+      email: "john.doe@example.com",
+      instagram: "john_doe_instagram",
+      linkedin: "john_doe_linkedin",
+    },
+  ];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -22,7 +56,36 @@ function Sports() {
   return (
     <>
       <Navbar scrolling={scrolling} />
-      <div>Teams & Clubs</div>
+      <div
+        className=""
+        style={{ background: "linear-gradient(to right, #ffe270, #ff914d)" }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: "31%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            fontSize: "16vw",
+            color: "antiquewhite",
+          }}
+        >
+          Sports
+        </div>
+        <div className="flex items-center flex-wrap justify-center gap-20 pt-60 pb-20 px-10">
+          {data.map((member) => (
+            <div>
+              <TeamCard
+                name={member.name}
+                image={member.imageUrl}
+                gmail={member.email}
+                instagram={member.instagram}
+                linkedin={member.linkedin}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
