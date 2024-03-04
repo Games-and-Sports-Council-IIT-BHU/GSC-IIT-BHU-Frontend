1e03cb51051a74e43c9981c5dcd0af0c55e61f8c
Rashmik <rashmik2705@gmail.com>
2024-03-05 01:36:07 +0530

    Add Events hero section for mobile

--- gsc-frontend/src/components/Events/EventsHero.jsx
+++ gsc-frontend/src/components/Events/EventsHero.jsx
@@ -4,8 +4,8 @@ const EventsHero = () => {
   return (
     <div className="bg-[url('/assets/Events/BlueBackground.svg')] bg-no-repeat bg-cover">
       <div className="bg-[url('/assets/Events/Group.svg')] bg-no-repeat bg-cover bg-bottom">
-      <div className="bg-[url('/assets/Events/Torch.svg')] bg-no-repeat bg-cover bg-left-bottom  bg-[length:260px] sm:bg-[length:360px] md:bg-left-bottom-4 md:bg-[length:480px]">
-        <div className='py-60 sm:pb-72 sm:pt-48 md:pb-80 md:pt-56  flex justify-center'>
+      <div className="bg-[url('/assets/Events/Torch.svg')] bg-no-repeat bg-cover bg-left-bottom  bg-[length:270px] ss:bg-[length:320px] md:bg-left-bottom-3 smd:bg-[length:410px] ">
+        <div className=' pb-60 pt-44 ss:pt-52 ss:pb-56   sm:pb-72 sm:pt-48  md:pb-80 md:pt-56 flex justify-center'>
         <img src={Events} className='size-3/5 sm:size-3/6  md:size-2/5' />
         </div>
         </div>
